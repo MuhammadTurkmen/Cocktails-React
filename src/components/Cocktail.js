d06aa9de03cefc8e4f991d3136c9c53e084da0bc
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 09:20:44 +0430

    init

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -10,6 +10,7 @@ const Cocktail = ({image, name, id, info, glass}) => {
       <div className="cocktail-footer">
         <h3>{name}</h3>
         <h4>{glass}</h4>
+        <p>{info}</p>
       </div>
     </article>
   )
