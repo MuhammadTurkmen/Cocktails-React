788212d4965be5df0d3e0c5b6bc7cb2a46875609
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:45:06 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,10 +10,14 @@ const Navbar = () => {
           <img src={logo} alt="cocktail db logo" className='logo'/>
         </Link>
         <ul className='nav-links'>
-
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </div>
-      <h2>navbar component</h2>
     </nav>
   )
 }
