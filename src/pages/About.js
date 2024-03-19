61c94fd5d25cc53adcf7a208db107888b075b1f3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:53:02 +0430

    init

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,13 @@ const About = () => {
   return (
     <section className='section about-section'> 
       <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, explicabo 
+        voluptatibus? Enim dignissimos asperiores exercitationem natus sit. Optio perspiciatis, 
+        aspernatur, magni ipsa quae in, nesciunt minus temporibus aliquam expedita ex asperiores dolorem. 
+        Necessitatibus odit molestias 
+        reiciendis architecto non voluptas, debitis inventore eius nobis nesciunt?
+      </p>
     </section>
   )
 }
