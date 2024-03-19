01b54e758bf7498835e4cafe1de837eeea896db6
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:56:10 +0430

    init

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,9 +6,8 @@ const Error = () => {
     <section className='error-page section'>
       <div className="error-container">
         <h1>oops! it's a dead end</h1>
-      </div>
-      <h2>error page</h2>
-      
+        <Link to='/' className='btn btn-primary'>back home</Link>
+      </div>      
     </section>
   )
 }
