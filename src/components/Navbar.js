c04542498421156d4fe1e342ed1c0bb0f381a029
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:43:08 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className='navbar'>
       <div className="nav-center">
         <Link to='/'>
-          <img src={logo} alt="cocktail db logo" />
+          <img src={logo} alt="cocktail db logo" className='logo'/>
         </Link>
       </div>
       <h2>navbar component</h2>
