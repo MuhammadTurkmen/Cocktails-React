fb7400bc64eae72e3d170bfbf31481b1637b3d4e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:54:56 +0430

    init

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const Error = () => {
   return (
     <section className='error-page section'>
-      <div className="error-container"></div>
+      <div className="error-container">
+        <h1>oops! it's a dead end</h1>
+      </div>
       <h2>error page</h2>
       
     </section>
