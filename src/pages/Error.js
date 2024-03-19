d5ca6141c806d435161aa2cb3ba709484e376e59
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:54:30 +0430

    init

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const Error = () => {
   return (
     <section className='error-page section'>
+      <div className="error-container"></div>
       <h2>error page</h2>
+      
     </section>
   )
 }
