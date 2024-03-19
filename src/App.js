798277974ba81e0564ea9b8d5d21346f83eae1e9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:36:20 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
@@ -25,7 +25,7 @@ function App() {
       </Route>
 
       <Route path='*'>
-        <Error />
+        <Error   />
       </Route>
     </Switch>
    </Router>
