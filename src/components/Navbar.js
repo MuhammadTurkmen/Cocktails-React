45d2980b950d0917efc1f326d0a0793ef9455090
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 10:41:18 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ import logo from '../logo.svg'
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      
+      <div className="nav-center">
+        
+      </div>
       <h2>navbar component</h2>
     </nav>
   )
