018a35a42d7393b05e872b4bc74841bcd2cb2452
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-19 20:49:29 +0430

    init

--- src/context.js
+++ src/context.js
@@ -15,6 +15,7 @@ const AppProvider = ({ children }) => {
     try {
       const response = await fetch(`${url}${searchTerm}`)
       const data = await response.json()
+      const {drinks} = data
     } catch (error) {
       console.log(error);
     }
