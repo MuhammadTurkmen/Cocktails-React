3e66573a1c9be394b1c2f8880924325c4e8d1af8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 07:39:04 +0430

    init

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -18,7 +18,7 @@ const CocktailList = () => {
     <section className='section'>
       <h2 className='section-title'>
         cocktails
-      </h2>
+      </h2> 
       <div className='cocktails-center'> 
         {cocktails.map((item) => {
           return <Cocktail key={item.id} {...item}/> 
