ddb21189c1eed88f3f0ccb69c1360fc91cfc4ba3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 09:38:11 +0430

    init

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -11,7 +11,9 @@ const Cocktail = ({image, name, id, info, glass}) => {
         <h3>{name}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>  
-        <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'/> 
+        <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
+          details
+        </Link> 
       </div>  
     </article>
   ) 
