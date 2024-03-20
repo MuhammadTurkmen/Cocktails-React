9b64aeea3ec432d6d7bf4b59984b88e54af7cc88
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 10:39:13 +0430

    init

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -92,7 +92,7 @@ const SingleCocktail = () => {
           <p>
             <span className='drink-data'>ingredients : </span>
             {ingredients.map((item, index) => {
-              
+              return item ? <span key={index}>{item}</span> : null
             })}
           </p>
         </div>
