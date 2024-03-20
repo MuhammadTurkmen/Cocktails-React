d6898fdffbb4f9dbbbc3af6002f7daec7b809fd3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 13:18:41 +0430

    init

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -14,9 +14,13 @@ const SearchForm = () => {
     setSearchTerm(searchValue.current.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <section className='section search'>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name"> search your favorite cocktail</label>
           <input type="text" id='name' ref={searchValue} onChange={searchCocktail}/>
