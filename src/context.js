57aac7d1937f0821c43b8f21889b7d092ad9c768
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 10:46:02 +0430

    init

--- src/context.js
+++ src/context.js
@@ -39,7 +39,7 @@ const AppProvider = ({ children }) => {
 
   useEffect(() => {
     fetchDrinks()
-  }, [searchTerm])
+  }, [searchTerm, fetchDrinks])
 
   return <AppContext.Provider value={{
     loading,
