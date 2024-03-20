4f14749b890722e0443858805ccf2551229c2061
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 11:49:05 +0430

    init

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,6 +4,7 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
   const {setSearchTerm} = useGlobalContext()
   const searchValue = React.useRef('')
+  
   return (
     <div>
       <h2>search form component</h2>
