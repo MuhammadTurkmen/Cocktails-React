20b3eba245c95d7640ceced9d058c9d1cc495581
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 11:57:45 +0430

    init

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -10,6 +10,7 @@ const SearchForm = () => {
       <form className="search-form">
         <div className="form-control">
           <label htmlFor="name"> search your favorite cocktail</label>
+          <input type="text" id='name' ref={searchValue}/>
         </div>
       </form>
     </section>
