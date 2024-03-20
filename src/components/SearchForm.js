2e2d95dbd08321af49faf0fc14b41eade15555b2
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 12:28:07 +0430

    init

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -10,7 +10,7 @@ const SearchForm = () => {
       <form className="search-form">
         <div className="form-control">
           <label htmlFor="name"> search your favorite cocktail</label>
-          <input type="text" id='name' ref={searchValue}/>
+          <input type="text" id='name' ref={searchValue} onChange={searchCocktail}/>
         </div>
       </form>
     </section>
