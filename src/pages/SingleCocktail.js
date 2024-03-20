5ab8a310433e4230e9b5372bbddffcf74d26f20c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 09:57:12 +0430

    init

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -4,6 +4,8 @@ import { useParams, Link } from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  const {id} = useParams()
+  
   return (
     <div>
       <h2>single cocktail page </h2>
