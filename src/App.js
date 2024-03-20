ff0c1a04dfc8a3103f5f1e58deaf368680e3ec57
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 10:51:32 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
@@ -11,23 +11,9 @@ function App() {
   return (
    <Router>
     <Navbar />
-    <Switch>
-      <Route exact path="/" >
-        <Home />
-      </Route>
-
-      <Route  path="/about" >
-        <About />
-      </Route>
-
-      <Route path="/cocktail/:id" >
-        <SingleCocktail />
-      </Route>
-
-      <Route path='*'>
-        <Error   />
-      </Route>
-    </Switch>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
    </Router>
   )
 }
