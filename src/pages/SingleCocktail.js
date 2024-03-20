f5f0cb0e8eb0a547d060e91dde1e67aab9c63876
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 10:00:05 +0430

    init

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -6,6 +6,8 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 const SingleCocktail = () => {
   const {id} = useParams()
   const [loading, setLoading] = React.useState(false)
+  const [cocktail, setCocktail] = React.useState(null)
+  
   return (
     <div>
       <h2>{id}</h2>
