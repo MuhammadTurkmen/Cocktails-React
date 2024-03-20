57f0cfee8449f8518bbdb33c12801942b6f0e3af
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-20 10:27:08 +0430

    init

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -66,6 +66,9 @@ const SingleCocktail = () => {
     <section className='section cocktail-section'>
       <Link to='/' className='btn btn-primary'>back home</Link>
       <h2 className='section-title'>{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+      </div>
     </section>
   )
 }
